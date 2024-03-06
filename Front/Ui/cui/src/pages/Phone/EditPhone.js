908e11b8fcbac0dfeb0460a93d8f521e3525b4bb
master <emami.javad@alis.ir>
2024-03-06 17:27:33 +0330

    v1

--- Front/Ui/cui/src/pages/Phone/EditPhone.js
+++ Front/Ui/cui/src/pages/Phone/EditPhone.js
@@ -1,14 +1,8 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
-import { useNavigate, useParams} from "react-router-dom";
-import Box from "@mui/material/Box";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import SendIcon from "@mui/icons-material/Send";
+import {useNavigate, useParams} from "react-router-dom";
+import {Button, Label, TextInput, Select, Badge} from 'flowbite-react';
+
 
 export default function EditPhone() {
     const navigate = useNavigate();
@@ -17,10 +11,10 @@ export default function EditPhone() {
 
     useEffect(() => {
         getPhone();
-    },[]);
+    }, []);
 
     function getPhone() {
-        axios.get(`http://localhost:5000/phonedetails/${id_phone}`).then(function(response)  {
+        axios.get(`http://localhost:5000/phonedetails/${id_phone}`).then(function (response) {
             console.log(response.data);
             setInputs(response.data);
         });
@@ -34,95 +28,96 @@ export default function EditPhone() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        axios.put(`http://localhost:5000/phoneupdate/${id_phone}`, inputs).then(function(response) {
+        axios.put(`http://localhost:5000/phoneupdate/${id_phone}`, inputs).then(function (response) {
             console.log(response.data);
             navigate('/');
         });
     }
 
     return (
-        <div>
-            <div className="container h-100">
-                <div className="row">
-                    <div className="col-2"></div>
-                    <div className="col-8">
-                        <h1>Edit Phone</h1>
-                        <Box sx={{minWidth: 120}}>
-                            <form onSubmit={handleSubmit}>
-                                <FormControl fullWidth>
-                                    <div className="mb-3">
-                                        <InputLabel name="router"
-                                                    id="demo-simple-select-helper-label">Router</InputLabel>
-                                            <Select
-                                                    displayEmpty
-                                                    name="routerpe"
-                                                    labelId="ddemo-simple-select-helper-label"
-                                                    id="demo-simple-select-helper"
-                                                    value={inputs.routerpe}
-                                                    label="Router"
-                                                    onChange={handleChange}
-                                            >
-                                                <MenuItem value="" name="routerpe">
-                                                    <em>None</em>
-                                                </MenuItem>
-                                                <MenuItem name="routerpe" value={inputs.routerpe}>{inputs.routerpe}
-                                                </MenuItem>
-                                            </Select>
-                                    </div>
-                                    <div className="mb-3">
-                                        <TextField name="tagpe" value={inputs.tagpe} id="outlined-basic" label="Phone Tag" variant="outlined"
-                                                   onChange={handleChange}/>
-                                    </div>
-                                    <div className="mb-3">
-                                        <TextField name="ownerpe" value={inputs.ownerpe} id="outlined-basic" label="Phone Owner"
-                                                   variant="outlined"
-                                                   onChange={handleChange}/>
-                                    </div>
-                                    <div className="mb-3">
-                                        <TextField name="typepe" value={inputs.typepe} id="outlined-basic" label="Phone Type"
-                                                   variant="outlined"
-                                                   onChange={handleChange}/>
-                                    </div>
-                                    <div className="mb-3">
-                                        <TextField name="linepe" value={inputs.linepe} id="outlined-basic" label="Line Number"
-                                                   variant="outlined"
-                                                   onChange={handleChange}/>
-                                    </div>
-                                    <div className="mb-3">
-                                        <TextField name="idmacpe" value={inputs.idmacpe} id="outlined-basic" label="Phone Name"
-                                                   variant="outlined"
-                                                   onChange={handleChange}/>
-                                    </div>
-                                    <div className="mb-3">
-                                        <TextField name="userdnpe" value={inputs.userdnpe} id="outlined-basic" label="Digist User"
-                                                   variant="outlined"
-                                                   onChange={handleChange}/>
-                                    </div>
-                                    <div className="mb-3">
-                                        <TextField name="passdnpe" value={inputs.passdnpe} id="outlined-basic" label="Digist Password"
-                                                   variant="outlined"
-                                                   onChange={handleChange}/>
-                                    </div>
-                                    <div className="mb-3">
-                                        <TextField name="codecpe" value={inputs.codecpe} id="outlined-basic" label="Voice Codec"
-                                                   variant="outlined"
-                                                   onChange={handleChange}/>
-                                    </div>
-                                    <div className="mb-3">
-                                        <TextField name="vcodecpe" value={inputs.vcodecpe} id="outlined-basic" label="Class Codec"
-                                                   variant="outlined"
-                                                   onChange={handleChange}/>
-                                    </div>
-                                    <div className="mb-3">
-                                        <Button endIcon={<SendIcon/>} type="submit" name="add" variant="contained"
-                                                label="Add Phone"></Button>
-                                    </div>
-                                </FormControl>
-                            </form>
-                        </Box>
-                    </div>
-                    <div className="col-2"></div>
+        <div className="container">
+            <div className="row">
+                <div className="col-3"></div>
+                <div className="col-8">
+                    <h2 class="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">
+                        <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">PHONE EDIT</span>
+                    </h2>
+                    <form className="flex max-w-md flex-col gap-4" onSubmit={handleSubmit}>
+                        <div className="max-w-md gap-2">
+                            <div className="mb-2 block">
+                                <Label htmlFor="router" value="Select Router"/>
+
+                                <Select id="routers" required name="routerpe">
+                                    <option>{inputs.routerpe}</option>
+                                    <option>None</option>
+                                </Select>
+                            </div>
+                            <div className="mb-2 block">
+                                <Label htmlFor="tag" value="Phone Tag"/>
+                                <TextInput id="tape" type="text" placeholder="Phone Tag" name="tagpe"
+                                           value={inputs.tagpe}
+                                           onChange={handleChange} required/>
+                            </div>
+                            <div className="mb-2 block">
+                                <Label htmlFor="owner" value="Phone Owner"/>
+                                <TextInput id="ownerpe" type="text" placeholder="Phone Owner" name="ownerpe"
+                                           value={inputs.ownerpe}
+                                           onChange={handleChange} required/>
+                            </div>
+                            <div className="mb-2 block">
+                                <Label htmlFor="type" value="Phone Type"/>
+                                <TextInput id="typepe" type="text" placeholder="Phone Type" name="typepe"
+                                           value={inputs.typepe}
+                                           onChange={handleChange} required/>
+                            </div>
+                            <div className="mb-2 block">
+                                <Label htmlFor="line" value="Line Number"/>
+                                <TextInput id="linepe" type="text" placeholder="Line Number" name="linepe"
+                                           value={inputs.linepe}
+                                           onChange={handleChange} required/>
+                            </div>
+                            <div className="mb-2 block">
+                                <Label htmlFor="Phone Name" value="Phone Name"/>
+                                <TextInput id="idmacpe" type="text" placeholder="Phone Name" name="idmacpe"
+                                           value={inputs.idmacpe}
+                                           onChange={handleChange} required/>
+                            </div>
+                            <div className="mb-2 block">
+                                <Label htmlFor="Digist User" value="Digist User"/>
+                                <TextInput id="userdnpe" type="text" placeholder="Digist User" name="userdnpe"
+                                           value={inputs.userdnpe}
+                                           onChange={handleChange} required/>
+                            </div>
+                            <div className="mb-2 block">
+                                <Label htmlFor="Digist Pass" value="Digist Pass"/>
+                                <TextInput id="passdnpe" type="text" placeholder="Digist User" name="passdnpe"
+                                           value={inputs.passdnpe}
+                                           onChange={handleChange} required/>
+                            </div>
+                            <div className="mb-2 block">
+                                <Label htmlFor="Voice Codec" value="Voice Codec"/>
+                                <TextInput id="codecpe" type="text" placeholder="Voice Codec" name="codecpe"
+                                           value={inputs.codecpe}
+                                           onChange={handleChange} required/>
+                            </div>
+                            <div className="mb-2 block">
+                                <Label htmlFor="Class Codec" value="Class Codec"/>
+                                <TextInput id="vcodecpe" type="text" placeholder="Class Codec" name="vcodecpe"
+                                           value={inputs.vcodecpe}
+                                           onChange={handleChange} required/>
+                            </div>
+                            <div className="mb-2 block">
+                                <br/>
+                            </div>
+                            <div className="flex flex-wrap gap-3">
+                                <Button gradientDuoTone="greenToBlue" type="submit" name="add">Add Phone</Button>
+                             </div>
+                        </div>
+
+                    </form>
                 </div>
+                <div className="col-3"></div>
+
             </div>
         </div>
     );
